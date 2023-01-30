73f0a8943e995275a8141d4165c1c37f08d8bc30
antiLimited <antilimitedmail@gmail.com>
2023-01-29 22:31:45 -0500

    Minor UI Changes (Topbar Added)

--- sfsPlaceGame/src/gui.js
+++ sfsPlaceGame/src/gui.js
@@ -18,7 +18,6 @@ var partsPlaced = []
 var hoverPart = ""
 var selectPart = ""
 
-
 export function drawGui(k) {
     drawRect({
         width: k.width(),
@@ -29,10 +28,32 @@ export function drawGui(k) {
         origin: "center"
     })
 
+    drawRect({
+        width: k.width() * 0.95,
+        height: k.height() * 0.375,
+        pos: toWorld(vec2(k.width() / 2, 0)),
+        opacity: 0.5,
+        radius: 32,
+        color: BLACK,
+        outline: { color: BLACK, width: 4 },
+        origin: "center"
+    })
+
+    drawText({
+        text: "sfsPlace",
+        size: 96,
+        font: "sink",
+        width: k.width() / 2,
+        pos: toWorld(vec2(k.width() / 2, k.height() * 0.09375)),
+        origin: "center",
+        color: WHITE,
+    })
+
     if (window.parts == undefined) {
 
     } else {
         var indent = 0
+        var hoverCount = 0
         for (let part of window.parts) {
             var position = toWorld(vec2(((k.width() + 64) - k.width()) + (indent * 128), k.height() - 64))
 
@@ -40,11 +61,17 @@ export function drawGui(k) {
                 width: 96,
                 height: 96,
                 pos: position,
-                opacity: 0.5,
+                opacity: (() => {
+                    if (hoverPart == part.name) {
+                        return 0.3
+                    } else {
+                        return 0.6
+                    }
+                })(),
                 radius: 16,
                 color: (() => {
-                    if (hoverPart == part.name) {
-                        return GREEN
+                    if (selectPart == part.name) {
+                        return CYAN
                     } else {
                         return BLACK
                     }
@@ -59,6 +86,8 @@ export function drawGui(k) {
                 if (isMouseDown("left")) {
                     selectPart = part.name
                 }
+            } else {
+                hoverCount += 1
             }
 
             if (part.width >= part.height) {
@@ -78,11 +107,14 @@ export function drawGui(k) {
             }
             indent += 1
         }
+        if (hoverCount == window.parts.length) {
+            hoverPart = ""
+        }
     }
 }
 
 export function drawPlacedParts(k) {
-    if (isMousePressed("left")){
+    if (isMousePressed("left")) {
         var mouseClickPos = toWorld(mousePos())
         debug.log(Math.round(mouseClickPos.x / 32) + " " + Math.round(mouseClickPos.y / 32))
         if (mousePos().y < k.height() - 128 && selectPart != "") {
